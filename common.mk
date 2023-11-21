798a62bb3f38b302dccaae9ca6be25b4b9a85d93
tqmane <149690876+tqmane@users.noreply.github.com>
2023-11-21 22:56:13 +0900

    sdm845-common: build libprotobuf-cpp-full-3.9.1 and ship prebuilt lib… …protobuf-cpp-full
    * libprotobuf-cpp-full-vendorcompat is no longer a thing

--- common.mk
+++ common.mk
@@ -168,4 +168,12 @@ PRODUCT_COPY_FILES += \
 # APEX
 PRODUCT_COMPRESSED_APEX := false
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
+
 $(call inherit-product, vendor/samsung/sdm845-common/sdm845-common-vendor.mk)
