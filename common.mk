6fe4951d729a198d99bea809df25c452034f335e
tqmane <149690876+tqmane@users.noreply.github.com>
2023-12-17 23:59:31 +0900

    sdm845-common: some change 2

--- common.mk
+++ common.mk
@@ -158,10 +158,10 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.bluetooth.default \
     audio.primary.sdm845 \
